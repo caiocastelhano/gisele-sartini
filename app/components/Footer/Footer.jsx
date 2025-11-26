fe8fc36cf6e3b49f213bf21396693e148ac32263
Caio Castelhano <castelhanoc@gmail.com>
2025-11-26 16:31:32 -0300

    Create compact footer design

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -1,14 +1,16 @@
+import styles from "./Footer.module.css";
+
 export default function Footer({ dictionary }) {
   return (
-    <footer>
-      <p>
+    <footer className={styles.footer}>
+      <p className={styles.text}>
         © {new Date().getFullYear()} Gisele Sartini —{" "}
-        {dictionary.footer.rights}{" "}
-        {dictionary.footer.developed}{" "}
+        {dictionary.footer.rights} {dictionary.footer.developed}{" "}
         <a
           href="https://caiocastelhano.com.br"
           target="_blank"
           rel="noopener noreferrer"
+          className={styles.link}
         >
           Caio Castelhano
         </a>
