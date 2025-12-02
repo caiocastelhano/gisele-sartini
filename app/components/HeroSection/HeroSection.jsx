1a76176d569ae1bc5d9458d25c703c2e6a830b2e
Caio Castelhano <castelhanoc@gmail.com>
2025-12-02 18:26:30 -0300

    Add HeroSection with local video (not the real one)

--- app/components/HeroSection/HeroSection.jsx
+++ app/components/HeroSection/HeroSection.jsx
@@ -1,7 +1,16 @@
+import styles from "./HeroSection.module.css";
+
 export default function HeroSection() {
   return (
-    <section>
-      <p>Demo reel</p>
+    <section className={styles.heroSection}>
+      <video
+        className={styles.video}
+        src="/videos/videoprovisorio.mp4"
+        autoPlay
+        loop
+        muted
+        playsInline
+      ></video>
     </section>
   );
 }
