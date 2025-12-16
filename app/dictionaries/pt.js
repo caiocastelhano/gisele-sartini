426c5b7ab8990206fc671d05b0f409b1155dc7ce
Caio Castelhano <castelhanoc@gmail.com>
2025-12-16 11:00:25 -0300

    Add video categories (PT & EN)

--- app/dictionaries/pt.js
+++ app/dictionaries/pt.js
@@ -226,211 +226,211 @@ export default {
         src: "https://www.youtube.com/embed/XTXKzDz3Tb0",
         title: "Bolo de Chocolate da chef Patrícia Figueiredo l TV Gazeta",
         caption: "Bolo de Chocolate da chef Patrícia Figueiredo l TV Gazeta",
-        category: "categoria1"
+        category: "Youtube"
       },
       {
         src: "https://www.youtube.com/embed/tD3FTAZ8was",
         title: "SÃO SILVESTRE: entrevista EXCLUSIVA com Paul Tergat, maior campeão da prova",
         caption: "SÃO SILVESTRE: entrevista EXCLUSIVA com Paul Tergat, maior campeão da prova",
-        category: "categoria1"
+        category: "Entrevistas"
       },
       {
         src: "https://www.youtube.com/embed/iLvTiLUdUp4",
         title: "PRÉ-JOGO: Corinthians x Palmeiras na Copa do Brasil + DANIEL FURLAN AO VIVO | #TáNoJogo #80",
         caption: "PRÉ-JOGO: Corinthians x Palmeiras na Copa do Brasil + DANIEL FURLAN AO VIVO | #TáNoJogo #80",
-        category: "categoria1"
+        category: "Livestream"
       },
       {
         src: "https://www.youtube.com/embed/ZRl07ekvBko",
         title: "PÉRICLES - Entrevista Exclusiva | Gazeta FM",
         caption: "PÉRICLES - Entrevista Exclusiva | Gazeta FM",
-        category: "categoria1"
+        category: "Entrevistas"
       },
       {
         src: "https://www.youtube.com/embed/F1IMKT-lVOM",
         title: "Pauta e prosa #24: Espiritualidade na era digital | Jornal da Gazeta",
         caption: "Pauta e prosa #24: Espiritualidade na era digital | Jornal da Gazeta",
-        category: "categoria1"
+        category: "Podcasts"
       },
       {
         src: "https://www.youtube.com/embed/DhF7f-kgS7c",
         title: "Pode não parecer, mas é TRABALHO: Carnaval e Jornalismo de Entretenimento | Central 900 - #Ep2",
         caption: "Pode não parecer, mas é TRABALHO: Carnaval e Jornalismo de Entretenimento | Central 900 - #Ep2",
-        category: "categoria1"
+        category: "Podcasts"
       },
       {
         src: "https://www.youtube.com/embed/-dfpISBxUww",
         title: "ITUB4 entrega recorde, IGTI11 surpreende e ROXO34 promete; é hora de comprar ou vender? | Money Times",
         caption: "ITUB4 entrega recorde, IGTI11 surpreende e ROXO34 promete; é hora de comprar ou vender? | Money Times",
-        category: "categoria1"
+        category: "Youtube"
       },
       {
         src: "https://www.youtube.com/embed/7Ke3ZIy5EAE",
         title: "Para de falar com a sua mãe | Mina Bem-estar",
         caption: "Para de falar com a sua mãe | Mina Bem-estar",
-        category: "categoria2"
+        category: "Redes sociais"
       },
       {
         src: "https://www.youtube.com/embed/n3jtNVomhDE",
         title: "VLOG Crica + Banco Pan",
         caption: "VLOG Crica + Banco Pan",
-        category: "categoria2"
+        category: "Redes sociais"
       },
       {
         src: "https://www.youtube.com/embed/T6GsH1hQhRU",
         title: "CONVIDADO: O Brasil que deu certo + ESTREIA de Ancelotti + INVASÃO no Corinthians | #TáNoJogo #65",
         caption: "CONVIDADO: O Brasil que deu certo + ESTREIA de Ancelotti + INVASÃO no Corinthians | #TáNoJogo #65",
-        category: "categoria2"
+        category: "Livestream"
       },
       {
         src: "https://www.youtube.com/embed/EpAnyncIW3U",
         title: "GLORIA GROOVE - Entrevista Exclusiva",
         caption: "GLORIA GROOVE - Entrevista Exclusiva",
-        category: "categoria2"
+        category: "Entrevistas"
       },
       {
         src: "https://www.youtube.com/embed/O_gIYrPSbpg",
         title: "THIAGUINHO - Entrevista Exclusiva",
         caption: "THIAGUINHO - Entrevista Exclusiva",
-        category: "categoria2"
+        category: "Entrevistas"
       },
       {
         src: "https://www.youtube.com/embed/xpLYHYKJuwk",
         title: "DROPS DE ANIS #8 - Leona Cavalli, a arte é o ofício",
         caption: "DROPS DE ANIS #8 - Leona Cavalli, a arte é o ofício",
-        category: "categoria2"
+        category: "Podcasts"
       },
       {
         src: "https://www.youtube.com/embed/B27uCsWGtX0",
         title: "DROPS DE ANIS #4 - Um bate papo com a #atriz Virgínia Cavendish",
         caption: "DROPS DE ANIS #4 - Um bate papo com a #atriz Virgínia Cavendish",
-        category: "categoria2"
+        category: "Podcasts"
       },
       {
         src: "https://www.youtube.com/embed/3tURaF8PtBU",
         title: "Corte DROPS DE ANIS #3, com Leonardo Miggiorin",
         caption: "Corte DROPS DE ANIS #3, com Leonardo Miggiorin",
-        category: "categoria3"
+        category: "Podcasts"
       },
       {
         src: "https://www.youtube.com/embed/EXi7NwNXrcI",
         title: "Pauta e Prosa #23: O Tempo e a Felicidade | Mario Sérgio Cortella",
         caption: "Pauta e Prosa #23: O Tempo e a Felicidade | Mario Sérgio Cortella",
-        category: "categoria3"
+        category: "Podcasts"
       },
       {
         src: "https://www.youtube.com/embed/Dz010IZ-EEU",
         title: "O que nos resta? | 1º Bodybuilder Vegan Pro do Mundo",
         caption: "O que nos resta? | 1º Bodybodybuilder Vegan Pro do Mundo",
-        category: "categoria3"
+        category: "Podcasts"
       },
       {
         src: "https://www.youtube.com/embed/XecONcv9Vas",
         title: "JORNALISTA e INFLUENCIADORA DIGITAL: o melhor (e os desafios) dos dois mundos | Central 900 - #Ep18",
         caption: "JORNALISTA e INFLUENCIADORA DIGITAL: o melhor (e os desafios) dos dois mundos | Central 900 - #Ep18",
-        category: "categoria3"
+        category: "Podcasts"
       },
       {
         src: "https://www.youtube.com/embed/ZWRtHH-8crA",
         title: "Em busca da FAMA: Media Training e Reality Shows | Central 900 - #Ep1",
         caption: "Em busca da FAMA: Media Training e Reality Shows | Central 900 - #Ep1",
-        category: "categoria3"
+        category: "Podcasts"
       },
       {
         src: "https://www.youtube.com/embed/zhJi1uWoP_Q",
         title: "Um ano sem Paulo Gustavo: Angélica conversa com Ingrid Guimarães",
         caption: "Um ano sem Paulo Gustavo: Angélica conversa com Ingrid Guimarães",
-        category: "categoria3"
+        category: "Youtube"
       },
       {
         src: "https://www.youtube.com/embed/XI5_8_NC8TY",
         title: "Dandara Pagu: É melhor ser casada ou solteira?",
         caption: "Dandara Pagu: É melhor ser casada ou solteira?",
-        category: "categoria3"
+        category: "Youtube"
       },
       {
         src: "https://www.youtube.com/embed/XjDrxgJsoFY?list=PL9i60BZmDe5gHidCN53ThbYSANQsoiaM7",
         title: "Barbosa: Defendendo um Legado",
         caption: "Barbosa: Defendendo um Legado",
-        category: "categoria4"
+        category: "Podcasts"
       },
       {
         src: "https://www.youtube.com/embed/VUQ-Dkyl7Mc",
         title: "Brexit é bom para o Ibovespa? - Pergunta que eu Respondo",
         caption: "Brexit é bom para o Ibovespa? - Pergunta que eu Respondo",
-        category: "categoria4"
+        category: "Youtube"
       },
       {
         src: "https://www.youtube.com/embed/tpR0yFyZqIc",
         title: "CICATRIZ (Trailer) | Direção: Rodrigo Ribeiro Vieira",
         caption: "CICATRIZ (Trailer) | Direção: Rodrigo Ribeiro Vieira",
-        category: "categoria4"
+        category: "Projetos autorais"
       },
       {
         src: "https://www.youtube.com/embed/PusxWWw382o",
         title: "Entre Latas e Luta: a cena das mulheres no graffiti de São Paulo",
         caption: "Entre Latas e Luta: a cena das mulheres no graffiti de São Paulo",
-        category: "categoria4"
+        category: "Projetos autorais"
       },
       {
         src: "https://www.youtube.com/embed/fAM_2hlcO-A",
         title: "Autocuidado | Mina Bem-estar",
         caption: "Autocuidado | Mina Bem-estar",
-        category: "categoria4"
+        category: "Promocional"
       },
       {
         src: "https://www.youtube.com/embed/zsWDXBB2lYE",
         title: "Drinks zero álcool | Mina Bem-estar",
         caption: "Drinks zero álcool | Mina Bem-estar",
-        category: "categoria4"
+        category: "Redes sociais"
       },
       {
         src: "https://www.youtube.com/embed/leE3y1SpdpM",
         title: "Skincare | Mina Bem-estar",
         caption: "Skincare | Mina Bem-estar",
-        category: "categoria4"
+        category: "Promocional"
       },
       {
         src: "https://www.youtube.com/embed/ZYDBJQrM8Q0",
         title: "Bliss Bullet | Mina Bem-estar",
         caption: "Bliss Bullet | Mina Bem-estar",
-        category: "categoria5"
+        category: "Promocional"
       },
       {
         src: "https://www.youtube.com/embed/4PijXKmWy80",
         title: "De dentro para fora | Mina Bem-estar",
         caption: "De dentro para fora | Mina Bem-estar",
-        category: "categoria5"
+        category: "Promocional"
       },
       {
         src: "https://www.youtube.com/embed/VLN2x25Goz8",
         title: "Glow noturno | Mina Bem-estar",
         caption: "Glow noturno | Mina Bem-estar",
-        category: "categoria5"
+        category: "Promocional"
       },
       {
         src: "https://www.youtube.com/embed/PQs9hbw-Vwk",
         title: "Rafa Kalimann | Mina Bem-estar",
         caption: "Rafa Kalimann | Mina Bem-estar",
-        category: "categoria5"
+        category: "Redes sociais"
       },
       {
         src: "https://www.youtube.com/embed/x0rpdiwe9Iw",
         title: "S.O.S. TPM | Mina Bem-estar",
         caption: "S.O.S. TPM | Mina Bem-estar",
-        category: "categoria5"
+        category: "Promocional"
       },
       {
         src: "https://www.youtube.com/embed/XrYxmmzDDmU",
         title: "Pressão estética no trabalho | Mina Bem-estar",
         caption: "Pressão estética no trabalho | Mina Bem-estar",
-        category: "categoria5"
+        category: "Redes sociais"
       },
       {
         src: "https://www.youtube.com/embed/hCGz6Bf_gIs",
         title: "Mina Friday | Mina Bem-estar",
         caption: "Mina Friday | Mina Bem-estar",
-        category: "categoria6"
+        category: "Promocional"
       }
     ],
 
